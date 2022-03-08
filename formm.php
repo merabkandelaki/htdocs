7c9e048b9f857f3c26cc8be6e94f3603cfe21abf
merabkandelaki <kandelakimerab@gmail.com>
2022-03-08 20:47:42 +0400

    new update

---
+++ formm.php
@@ -0,0 +1,5 @@
+<!-- Пусть на странице formm.php с помощью формы у пользователя спрашивается пароль.  -->
+<form action="/result.php" methpd="POST">
+  <input type="password" name="pass">
+  <input type="submit">
+</form>
